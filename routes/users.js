06b9e547f50f4f9cc8baf73cff19e182f3467fc7
Samantha Carow <samantha.carow@gmail.com>
2015-10-11 16:49:09 -0700

    removed unnecessary code & comments

--- routes/users.js
+++ routes/users.js
@@ -4,25 +4,11 @@ var Users = require('../users.js').Users;
 var users = new Users();
 var cors = require('cors');
 
-/* GET users listing. */
 router.get('/', cors(), function(req, res) {
   var params = req.query;
   users.fetchUserBatch(params, function(error, users) {
     res.send(users);
   });
 });
-
-  // example for posting
-  // router.post('/:id', function(req, res) {
-  //   var _user = req.body;
-  //   _user._id = req.params.id;
-  //   users.updateUser(_user, function(error, user) {
-  //     if (user == null) {
-  //       res.send(error, 404);
-  //     } else {
-  //       res.send(user);
-  //     }
-  //   });
-  // });
 
 module.exports = router;
--- users.js
+++ users.js
@@ -5,19 +5,5 @@ Users = function() {
     var limit = parseInt(params.limit);
     cb(null, this.users.slice(offset, offset + limit));
   };
-
-  // example for posting
-  // this.updateUser = function(user, cb) {
-  //   this.fetchUserById(user._id, function(error, _user) {
-  //     if (error) {
-  //       cb(error, null);
-  //     } else {
-  //       _user.name = user.name;
-  //       _user.city = user.city;
-  //       _user.state = user.state;
-  //       cb(null, _user);
-  //     }
-  //   });
-  // };
 };
 exports.Users = Users;
